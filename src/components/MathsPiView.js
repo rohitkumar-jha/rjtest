7160d8232e08101deefcf81cf36ae494b0f7a98f
Rohitkumar Jha <58877946+rohitkumar-jha@users.noreply.github.com>
2024-07-07 23:36:02 +0530

    Add files via upload

---
+++ src/components/MathsPiView.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+const MathsPiView = () => {
+  return (
+    <div>
+      <h2>App Policy Statement</h2>
+      <div className="language-panel">
+        <h2>Introduction:</h2>
+        <p>
+          These Terms of Service govern your use of Maths Pi. By accessing or
+          using our app, you agree to be bound by these terms and our Privacy
+          Policy
+        </p>
+      </div>
+      <div className="language-panel">
+        <h2>User Responsibilities:</h2>
+        <h3>You agree to:</h3>
+        <p>
+          Provide accurate and complete registration information. Comply with
+          all applicable laws and regulations. Use Maths Pi only for lawful
+          purposes and in accordance with these Terms of Service.
+        </p>
+      </div>
+      <div className="language-panel">
+        <h2>Content and Services:</h2>
+        <h3>Maths Pi provides:</h3>
+        <p>
+          Topic Selection: Choose math topics to study, with access to reading
+          materials and examples. Quizzes and Exams: Take quizzes and exams
+          categorized as Easy, Medium, or Hard based on your selected topics.
+          Exam Results: View and filter your exam results to track your
+          performance.
+        </p>
+      </div>
+      <div className="language-panel">
+        <h2>Account Termination:</h2>
+        <p>
+          We reserve the right to suspend or terminate your account if you
+          violate these Terms of Service or engage in activities that disrupt or
+          threaten the security of Maths Pi or its users.
+        </p>
+      </div>
+      <div className="language-panel">
+        <h2>Limitation of Liability:</h2>
+        <p>
+          Maths Pi is provided "as is" without warranties of any kind. We are
+          not liable for any direct, indirect, incidental, special, or
+          consequential damages arising out of or in connection with your use of
+          Maths Pi.
+        </p>
+      </div>
+      <div className="language-panel">
+        <h2>Changes to Terms of Service</h2>
+        <p>
+          We may revise these Terms of Service from time to time. The most
+          current version will be posted within the app or otherwise
+          communicated to you. By continuing to use Maths Pi after changes are
+          made, you agree to be bound by the revised Terms of Service.
+        </p>
+        <p>
+          By using the Orderzz mobile application, you acknowledge that you have
+          read, understood, and agreed to be bound by this disclaimer. If you do
+          not agree with any part of this disclaimer, you must not use the App.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default MathsPiView;
